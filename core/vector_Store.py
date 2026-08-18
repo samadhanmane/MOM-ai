05267804ed5c9c594a9010a25a196c42d39aa980
Samadhan Mane <samadhanmane2324@gmail.com>
2026-08-18 21:24:33 +0530

    Switch vector embeddings to MistralAIEmbeddings to remove HuggingFace/PyTorch dependency

--- core/vector_Store.py
+++ core/vector_Store.py
@@ -1,36 +1,35 @@
 import os 
 from langchain_chroma import Chroma 
-from langchain_huggingface import HuggingFaceEmbeddings
+from langchain_mistralai import MistralAIEmbeddings
 from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain_core.documents import Document
 
 CHROMA_DIR = "vector_db"
 COLLECTION_NAME = "meeting_transcript"
-EMBEDDING_MODEL  = "all-MiniLM-L6-v2"
 
 def get_embeddings():
-    return HuggingFaceEmbeddings(
-        model_name = EMBEDDING_MODEL,
-        model_kwargs = {"device" : 'cpu'}
+    return MistralAIEmbeddings(
+        model="mistral-embed",
+        mistral_api_key=os.getenv("MISTRAL_API_KEY")
     )
 
-def build_vector_store(transcript : str)->Chroma:
-    print("Building vector Store")
+def build_vector_store(transcript: str) -> Chroma:
+    print("Building vector Store...")
 
     splitter = RecursiveCharacterTextSplitter(
-        chunk_size = 500,
-        chunk_overlap = 50
+        chunk_size=500,
+        chunk_overlap=50
     )
     chunks = splitter.split_text(transcript)
 
     docs = [
-        Document(page_content=chunk, metadata = {'chunk_index' : i})
-        for i,chunk in enumerate(chunks)
+        Document(page_content=chunk, metadata={'chunk_index': i})
+        for i, chunk in enumerate(chunks)
     ]
 
     embeddings = get_embeddings()
     vector_store = Chroma.from_documents(
-        documents= docs,
+        documents=docs,
         embedding=embeddings,
         collection_name=COLLECTION_NAME,
         persist_directory=CHROMA_DIR
@@ -39,20 +38,19 @@ def build_vector_store(transcript : str)->Chroma:
     return vector_store
 
 
-
-def load_vector_store() ->Chroma:
+def load_vector_store() -> Chroma:
     embeddings = get_embeddings()
     vector_store = Chroma(
         collection_name=COLLECTION_NAME,
-        embedding_function= embeddings,
+        embedding_function=embeddings,
         persist_directory=CHROMA_DIR
     )
 
     return vector_store
 
-def get_retriever(vector_store : Chroma, k :int = 4):
-    return vector_store.as_retriever(
-        search_type = 'similarity',
-        search_kwargs = {"k":k}
-    )
 
+def get_retriever(vector_store: Chroma, k: int = 4):
+    return vector_store.as_retriever(
+        search_type='similarity',
+        search_kwargs={"k": k}
+    )
